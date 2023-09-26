a34d32d1651e10ca4314f57c512520260568800b
admin62 <145646364+admin62@users.noreply.github.com>
2023-09-26 17:53:46 +0900

    AI의 도움을 받았습니다.

--- screenhoho/src/com/GraphicWindow.java
+++ screenhoho/src/com/GraphicWindow.java
@@ -2,39 +2,60 @@ package com;
 
 import java.awt.AWTException;
 import java.awt.Color;
-import java.awt.FlowLayout;
 import java.awt.Font;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class GraphicWindow extends JFrame {
-	/**
-	 * 
-	 */
-	static final long serialVersionUID = 1L;
-	JPanel panel;
-	JButton swc;
-	
-	public GraphicWindow() throws AWTException {
-		setSize(300, 200); //크기 설정
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setTitle("화면 호호기");
+    private static final long serialVersionUID = 1L;
+
+    private static final int FRAME_WIDTH = 300;
+    private static final int FRAME_HEIGHT = 200;
+
+    private static final int BUTTON_WIDTH = 150;
+    private static final int BUTTON_HEIGHT = 100;
+
+    private JPanel panel;
+    private JButton swcButton;
+
+    public GraphicWindow() throws AWTException {
+        setupFrame();
+        setupPanel();
+        add(panel);
+        setVisible(true);
+        
+        // Now that the frame is visible, we can setup the button.
+        setupButton();
+   }
+
+    private void setupFrame() {
+        setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("화면 호호기");
         setResizable(false);
-		setLayout(new FlowLayout()); //버튼 크기 임의조정
-		getContentPane().setBackground(Color.WHITE);
-		
-		swc = new JButton("실행중");
-		swc.addActionListener(new SwcListner());
-		swc.setFont(new Font("맑은 고딕", Font.PLAIN, 15));
-		swc.setVisible(true);
-		
-		panel = new JPanel();
-		panel.add(swc);
-		panel.setBackground(Color.WHITE);
-		
-		add(panel);
-		setVisible(true);
+        
+        getContentPane().setBackground(Color.WHITE);
+   }
+
+   private void setupPanel() {
+       panel = new JPanel();
+       panel.setLayout(null); // disable layout manager
+       panel.setBackground(Color.WHITE);
+   }
+
+   private void setupButton() throws AWTException {
+	    swcButton = new JButton("실행중");
+	    swcButton.addActionListener(new SwcListner());
+	    swcButton.setFont(new Font("맑은 고딕", Font.PLAIN, 15));
+
+	    // Calculate the position to center the button
+	    int xPos = (getWidth() - getInsets().left - getInsets().right - BUTTON_WIDTH) / 2;
+	    int yPos = (getHeight() - getInsets().top - getInsets().bottom - BUTTON_HEIGHT) / 2;
+
+	    swcButton.setBounds(xPos, yPos, BUTTON_WIDTH, BUTTON_HEIGHT);
+
+	    panel.add(swcButton); // Add button to the panel
 	}
+
 }
